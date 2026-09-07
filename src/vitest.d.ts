38a82b51fc74916ce0fef465d2019e8afed05749
Robin | Liquidium <github.com.thermal146@passmail.net>
2026-09-07 09:15:56 +0700

    test: use the Vitest DOM matcher integration

--- src/vitest.d.ts
+++
@@ -1,6 +0,0 @@
-import type { TestingLibraryMatchers } from '@testing-library/jest-dom/matchers';
-import 'vitest';
-
-declare module 'vitest' {
-  interface Matchers<R extends void | Promise<void>, T> extends TestingLibraryMatchers<T, R> {}
-}
